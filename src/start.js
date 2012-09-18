f6f346c2371ec2c0e448fcc4cfac844e13bc3ec7
zhiyu <zhengzhiyu@yeah.net>
2012-09-18 13:41:02 +0800

    udpate

--- src/start.js
+++ src/start.js
@@ -1,7 +1,13 @@
-var redisConfig = require('./redis');
-
+/*
+ Start Chat Server
+ */
 require('./chat_server');
 
+
+/*
+ Start Web Server
+ */
+var redisConfig = require('./redis');
 var icecream  = require('icecream');
 var port = 8000;
 icecream.createServer();
@@ -9,10 +15,19 @@ icecream.set('appRoot',  __dirname +'/app');
 icecream.set('debug', true);
 icecream.set('defaultEngine', 'ejs');
 
+//set redis for session storage 
 var connect    = require('connect');
 var RedisStore = require('connect-redis')(connect);
 var redis      = require("redis");
 var client     = redis.createClient(redisConfig.port,redisConfig.host);
 icecream.use(connect.session({ store: new RedisStore({client:client}), secret: '123' }));
+
+//not support IE
+icecream.use(function(req,res,next){
+        if(req.headers['user-agent']!=undefined && req.headers['user-agent'].toUpperCase().indexOf("MSIE")!=-1){
+             res.write("Sorry, Not Support IE!<br/><br/>Click to download <a href='http://chrome.google.com'>Chrome</a> or <a href='http://www.mozilla.com'>Firefox</a>");
+             res.end();
+        }else next();
+});
 icecream.listen(port);
-console.log('Listen on: ' + port);
+console.log('Web Server started, listening on : ' + port);
